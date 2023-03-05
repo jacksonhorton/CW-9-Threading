270d222e56facbe9ef3dabf1b31e99dc373e6de1
Jackson Horton <jacrhort@ut.utm.edu>
2023-03-05 15:08:17 -0600

    Mostly working

---
+++ CW9Threading/FindPiThread.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW9Threading
+{
+    internal class FindPiThread
+    {
+        private Random rand;
+        private int DartsToThrow;
+        private int DartsLandedInside;
+
+        public FindPiThread(int dartsToThrow)
+        {
+            rand = new Random();
+            DartsLandedInside = 0;
+            this.DartsToThrow = dartsToThrow;
+        }
+
+        public int GetDartsInside()
+        {
+            return DartsLandedInside;
+        }
+
+        private bool isInCircle(double x, double y)
+        {
+            // returns is a given point is within a circle (or quarter circle) of radius 1.
+            double radius = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+            return radius <= 1;
+        }
+
+        // needs to be public and void to correctly interact with Thread functions
+        public void throwDarts()
+        {
+            double x, y;
+            for (int i = 0; i < DartsToThrow; i++)
+            {
+                x = rand.NextDouble();
+                y = rand.NextDouble();
+
+                //Console.WriteLine(x + " " + y);
+                if (isInCircle(x, y))
+                {
+                    DartsLandedInside++;
+                }
+            }
+
+        }
+    }
+}
--- CW9Threading/Program.cs
+++ CW9Threading/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CW9Threading;
+using System.Threading;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // get user input
+        Console.Write("Enter number of darts to throw per thread and the number of threads to run (space separated): ");
+        string[] input = Console.ReadLine().Split();
+        int numDartsPerThread = int.Parse(input[0]);
+        int numThreads = int.Parse(input[1]);
+
+        // make lists for storing threads
+        List<Thread> threads = new();
+        List<FindPiThread> findPiThreads = new();
+
+        // loop 1: set up and start threads
+        for (int i = 0; i < numThreads; i++)
+        {
+            FindPiThread piThread = new(numDartsPerThread);
+            findPiThreads.Add(piThread);
+
+            Thread t = new Thread(new ThreadStart(piThread.throwDarts));
+            threads.Add(t);
+
+            t.Start();
+            Thread.Sleep(16);
+        }
+
+
+        // Loop 2: join all threads, waits for completion
+        foreach (var item in threads) { 
+            item.Join();
+        }
+
+
+        int numInside = 0;
+        // Loop 3: once all threads are complete, tally the num of darts that landed inside
+        foreach (var resultThread in findPiThreads)
+        {
+            numInside += resultThread.GetDartsInside();
+        }
+        
+
+        // Output Results
+        Console.WriteLine("Number of darts inside: " + numInside);
+        Console.WriteLine("Number of darts thrown: " + (numDartsPerThread * numThreads));
+
+        double piEstimate = 4 * (double) numInside / (numDartsPerThread * numThreads);
+        Console.WriteLine("Pi esitmate: " + piEstimate);
+
+    }
+}
